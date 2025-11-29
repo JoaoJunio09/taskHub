900e9133dad1d4e6e8f7e6ce3179cae07f69c00b
joao_junio <joaojunio818@gmail.com>
2025-11-29 15:04:30 -0300

    Método findAll

--- taskHub/src/main/java/com/joaojunio_dev/taskHub/controllers/PersonController.java
+++ taskHub/src/main/java/com/joaojunio_dev/taskHub/controllers/PersonController.java
@@ -1,11 +1,27 @@
 package com.joaojunio_dev.taskHub.controllers;
 
+import com.joaojunio_dev.taskHub.data.dto.PersonDTO;
+import com.joaojunio_dev.taskHub.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
+    @Autowired
+    private PersonService service;
 
+    @GetMapping(
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<PersonDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll());
+    }
 }
--- taskHub/src/main/java/com/joaojunio_dev/taskHub/services/PersonService.java
+++ taskHub/src/main/java/com/joaojunio_dev/taskHub/services/PersonService.java
@@ -1,14 +1,17 @@
 package com.joaojunio_dev.taskHub.services;
 
+import com.joaojunio_dev.taskHub.controllers.PersonController;
 import com.joaojunio_dev.taskHub.data.dto.PersonDTO;
-import com.joaojunio_dev.taskHub.mapper.ObjectMapper;
 import com.joaojunio_dev.taskHub.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import static com.joaojunio_dev.taskHub.mapper.ObjectMapper.parseObject;
 import static com.joaojunio_dev.taskHub.mapper.ObjectMapper.parseListObjects;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,7 +22,17 @@ public class PersonService {
 
     public List<PersonDTO> findAll() {
         var entities = repository.findAll();
-        var dto = parseListObjects(entities, PersonDTO.class);
-        return dto;
+        List<PersonDTO> dtos = new ArrayList<>();
+        parseListObjects(entities, PersonDTO.class).stream()
+            .map(dto -> {
+                addHateoas(dto);
+                dtos.add(dto);
+                return dto;
+            });
+        return dtos;
+    }
+
+    private void addHateoas(PersonDTO dto) {
+        dto.add(linkTo(methodOn(PersonController.class).findAll()).withRel("findAll").withType("GET"));
     }
 }
