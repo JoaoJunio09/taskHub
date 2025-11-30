e4c45ac29c9f33abbb32556bea4bd46dfc45a518
joao_junio <joaojunio818@gmail.com>
2025-11-30 11:44:38 -0300

    Adicionando logs no service

--- taskHub/src/main/java/com/joaojunio_dev/taskHub/services/PersonService.java
+++ taskHub/src/main/java/com/joaojunio_dev/taskHub/services/PersonService.java
@@ -6,6 +6,8 @@ import com.joaojunio_dev.taskHub.exceptions.NotFoundException;
 import com.joaojunio_dev.taskHub.exceptions.ObjectIsNullException;
 import com.joaojunio_dev.taskHub.model.Person;
 import com.joaojunio_dev.taskHub.repositories.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,10 +22,15 @@ import java.util.List;
 @Service
 public class PersonService {
 
+    private final Logger logger = LoggerFactory.getLogger(PersonService.class.getName());
+
     @Autowired
     PersonRepository repository;
 
     public List<PersonDTO> findAll() {
+
+        logger.info("Find's All Person");
+
         var entities = repository.findAll();
         List<PersonDTO> dtos = new ArrayList<>();
         parseListObjects(entities, PersonDTO.class).stream()
@@ -36,6 +43,9 @@ public class PersonService {
     }
 
     public PersonDTO findById(Long id) {
+
+        logger.info("Find a one Person");
+
         var entity = repository.findById(id)
             .orElseThrow(() -> new NotFoundException("Not found this ID : " + id));
         var dto = parseObject(entity, PersonDTO.class);
@@ -43,6 +53,9 @@ public class PersonService {
     }
 
     public PersonDTO create(PersonDTO dto) {
+
+        logger.info("Creating a one Person");
+
         if (dto == null) {
             throw new ObjectIsNullException("The Object is null");
         }
@@ -54,8 +67,10 @@ public class PersonService {
 
     public PersonDTO update(PersonDTO dto) {
 
+        logger.info("Updating a exists Person");
+
         var entity = repository.findById(dto.getId())
-                .orElseThrow(() -> new NotFoundException("Not found this ID:" + dto.getId()));
+            .orElseThrow(() -> new NotFoundException("Not found this ID:" + dto.getId()));
         entity.setFirstName(dto.getFirstName());
         entity.setLastName(dto.getLastName());
         entity.setBirthDate(dto.getBirthDate());
@@ -65,6 +80,9 @@ public class PersonService {
     }
 
     public void delete(Long id) {
+
+        logger.info("Deleting a one Person");
+
         var entity = repository.findById(id)
             .orElseThrow(() -> new NotFoundException("Not found this ID:" + id));
         repository.delete(entity);
