7f652994aac023a26732f850a3b0a3851a347c56
joao_junio <joaojunio818@gmail.com>
2025-12-20 18:56:57 -0300

    AuthController

---
+++ taskHub/src/main/java/com/joaojunio_dev/taskHub/controllers/AuthController.java
@@ -0,0 +1,66 @@
+package com.joaojunio_dev.taskHub.controllers;
+
+import com.joaojunio_dev.taskHub.data.dto.security.AccountCredentialsDTO;
+import com.joaojunio_dev.taskHub.mediatype.MediaType;
+import com.joaojunio_dev.taskHub.services.AuthService;
+import io.micrometer.common.util.StringUtils;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Authentication EndPoint!")
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+
+    @Autowired
+    private AuthService service;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signIn(@RequestBody AccountCredentialsDTO credentials) {
+        if (credentialsIsInvalid(credentials)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request");
+        var token = service.signIn(credentials);
+
+        if (token == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid cliente request");
+        return token;
+    }
+
+    @PutMapping("/refresh/{username}")
+    public ResponseEntity<?> refreshToken(
+        @PathVariable("username") String username,
+        @RequestHeader("Authorization") String refreshToken
+    ) {
+        if (parametersAreInvalid(username, refreshToken)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request");
+        var token = service.refreshToken(username, refreshToken);
+        if (token == null) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request");
+        return ResponseEntity.ok().body(token);
+    }
+
+    @PostMapping(
+        value = "/createUser",
+        produces = {
+            MediaType.APPLICATION_JSON,
+            MediaType.APPLICATION_XML,
+            MediaType.APPLICATION_YAML },
+        consumes = {
+            MediaType.APPLICATION_JSON,
+            MediaType.APPLICATION_XML,
+            MediaType.APPLICATION_YAML }
+    )
+    public AccountCredentialsDTO create(@RequestBody AccountCredentialsDTO credentials) {
+        return service.create(credentials);
+    }
+
+    boolean parametersAreInvalid(String username, String refreshToken) {
+        return StringUtils.isBlank(username) || StringUtils.isBlank(refreshToken);
+    }
+
+    static boolean credentialsIsInvalid(AccountCredentialsDTO credentials) {
+        return credentials == null ||
+            StringUtils.isBlank(credentials.getUsername()) ||
+            StringUtils.isBlank(credentials.getPassword());
+    }
+}
