2b8185b240fd3eb074a0ef23e45a4a92c7773995
joao_junio <joaojunio818@gmail.com>
2025-12-29 13:56:03 -0300

    Criando DTO's de PayloadDTO and SubscriptionDTO -> para enviar oo object da mensagem e a assinatura do usuário

---
+++ taskHub/src/main/java/com/joaojunio_dev/taskHub/data/dto/notification/PayloadDTO.java
@@ -0,0 +1,6 @@
+package com.joaojunio_dev.taskHub.data.dto.notification;
+
+public record PayloadDTO(
+    String title,
+    String body
+) {}
---
+++ taskHub/src/main/java/com/joaojunio_dev/taskHub/data/dto/notification/SubscriptionDTO.java
@@ -0,0 +1,7 @@
+package com.joaojunio_dev.taskHub.data.dto.notification;
+
+public record SubscriptionDTO(
+        String endpoint,
+        String p256dh,
+        String auth
+) {}
