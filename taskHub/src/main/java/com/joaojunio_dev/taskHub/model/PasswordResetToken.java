a8c19132b4f1526311b888932c0e3e44bd3316b0
joao_junio <joaojunio818@gmail.com>
2025-12-31 16:04:07 -0300

    Criando entidade para armazenar token de reset de senha

--- taskHub/src/main/java/com/joaojunio_dev/taskHub/model/PasswordResetToken.java
+++ taskHub/src/main/java/com/joaojunio_dev/taskHub/model/PasswordResetToken.java
@@ -1,4 +1,78 @@
 package com.joaojunio_dev.taskHub.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_password_reset_token")
 public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne
+    private Person person;
+
+    @Column
+    private LocalDateTime expiresAt;
+
+    public PasswordResetToken() {}
+
+    public PasswordResetToken(Long id, String token, Person person, LocalDateTime expiresAt) {
+        this.id = id;
+        this.token = token;
+        this.person = person;
+        this.expiresAt = expiresAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PasswordResetToken that = (PasswordResetToken) o;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
 }
