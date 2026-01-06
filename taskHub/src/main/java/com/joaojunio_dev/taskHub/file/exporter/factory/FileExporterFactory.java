64826e4f2da28e79781ed3ed40b565d7b08c0bad
joao_junio <joaojunio818@gmail.com>
2026-01-06 16:09:27 -0300

    Adicionando dependencias do Apache Poi e Commons Csv

--- taskHub/src/main/java/com/joaojunio_dev/taskHub/file/exporter/factory/FileExporterFactory.java
+++ taskHub/src/main/java/com/joaojunio_dev/taskHub/file/exporter/factory/FileExporterFactory.java
@@ -1,6 +1,9 @@
 package com.joaojunio_dev.taskHub.file.exporter.factory;
 
 import com.joaojunio_dev.taskHub.file.exporter.contract.TaskExporter;
+import com.joaojunio_dev.taskHub.file.exporter.impl.CsvExporterImpl;
+import com.joaojunio_dev.taskHub.file.exporter.impl.PdfExporterImpl;
+import com.joaojunio_dev.taskHub.file.exporter.impl.XlsxExporterImpl;
 import com.joaojunio_dev.taskHub.mediatype.MediaType;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +16,13 @@ public class FileExporterFactory {
 
     public TaskExporter getExporter(String acceptHeader) throws Exception {
         if (acceptHeader.equalsIgnoreCase(MediaType.APPLICATION_XLSX_VALUE)) {
-
+            return context.getBean(XlsxExporterImpl.class);
         }
         else if (acceptHeader.equalsIgnoreCase(MediaType.APPLICATION_CSV_VALUE)) {
-
+            return context.getBean(CsvExporterImpl.class);
         }
         else if (acceptHeader.equalsIgnoreCase(MediaType.APPLICATION_PDF_VALUE)) {
-
+            return context.getBean(PdfExporterImpl.class);
         }
         else {
             throw new BadRequestException("Invalid File Format!");
